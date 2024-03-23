d9f7c41b7d7679ab98af637775314b8020780071
zacisyourmaster <zac15590@gmail.com>
2024-03-23 16:22:53 -0700

    updated models and dbhelper and moved some things

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module monstertrio.myanime {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.slf4j;
+    requires java.sql;
 
     exports monstertrio.myanime.app;
     exports monstertrio.myanime.app.controllers;
--- src/main/java/monstertrio/myanime/app/models/User.java
+++ src/main/java/monstertrio/myanime/app/models/User.java
@@ -8,7 +8,10 @@ public class User {
 
     //constructor
     public User(){
-
+        id=0;
+        username="";
+        password="";
+        name="";
     }
     public User(String username, String password, String name) {
         this.username = username;
@@ -19,7 +22,6 @@ public class User {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -27,7 +29,6 @@ public class User {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
@@ -35,15 +36,12 @@ public class User {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
