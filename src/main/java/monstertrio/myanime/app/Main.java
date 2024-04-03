11035e70f7a1f9044e7a8da091e1bdd30ed077f4
zacisyourmaster <zac15590@gmail.com>
2024-04-03 09:13:03 -0700

    minor changes

--- src/main/java/monstertrio/myanime/app/Main.java
+++ src/main/java/monstertrio/myanime/app/Main.java
@@ -6,17 +6,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import monstertrio.myanime.app.helpers.DatabaseHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("AniTracker");
-        stage.setScene(scene);
-        stage.show();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("AniTracker");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            logger.error("",e);
+        }
     }
 
     public static void main(String[] args) {
