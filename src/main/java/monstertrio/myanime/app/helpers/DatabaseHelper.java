56132a00df7ff8ae16e57200bec3f013f3651373
zacisyourmaster <zac15590@gmail.com>
2024-04-03 09:12:03 -0700

    added addUser function

--- src/main/java/monstertrio/myanime/app/helpers/DatabaseHelper.java
+++ src/main/java/monstertrio/myanime/app/helpers/DatabaseHelper.java
@@ -1,14 +1,14 @@
 package monstertrio.myanime.app.helpers;
 
+import monstertrio.myanime.app.models.Anime;
+import monstertrio.myanime.app.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+
 public class DatabaseHelper {
-    private static final Logger logger = LoggerFactory.getLogger(ValidationHelper.class);
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseHelper.class);
     private static final String URL = "jdbc:sqlite:myanime.sqlite";
     private Connection conn=null;
 
@@ -21,7 +21,28 @@ public class DatabaseHelper {
         }
     }
 
+    //add User
+    public void addUser(User user) throws SQLException{
+        //check if user already exists
+        PreparedStatement psCheckAvail=conn.prepareStatement("SELECT * FROM users WHERE username=?");
+        psCheckAvail.setString(1,user.getUsername());
+        ResultSet resultSet = psCheckAvail.executeQuery();
+        if(resultSet.isBeforeFirst()){
+            System.out.println("User already exists!");
+            //javafx alert
+        } else{
+        String query="INSERT INTO users(name, username, password) VALUES(?,?,?)";
+        PreparedStatement psInsert = conn.prepareStatement(query);
+        psInsert.setString(1, user.getName());
+        psInsert.setString(2,user.getUsername());
+        psInsert.setString(3, user.getPassword());
+        psInsert.executeUpdate();
+        }
+    }
     //addAnime
+    public void addAnime(Anime anime){
+
+    }
     //updateAnime
     //deleteAnime
     public void closeConnection() {
