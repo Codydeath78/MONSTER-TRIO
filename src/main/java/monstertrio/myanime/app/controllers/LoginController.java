e874dfabbae0ce91f8cc91ed0632693aa7915f07
Codydeath78 <GOGETALIFEREALLYDUDE@gmail.com>
2024-04-03 22:44:06 -0700

    Minor fixes.

--- src/main/java/monstertrio/myanime/app/controllers/LoginController.java
+++ src/main/java/monstertrio/myanime/app/controllers/LoginController.java
@@ -1,4 +1,44 @@
 package monstertrio.myanime.app.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    ImageView myLoginImageView;
+    Image myLoginImage;
+
+    public LoginController() {
+        myLoginImage = new Image(getClass().getResourceAsStream("/images/chopcrycover.jpg"));
+
+    }
+    public void displayLoginImage(){
+        myLoginImageView.setImage(myLoginImage);
+    }
+
+    public void switchToLogin(ActionEvent event) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/Login.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
 }
