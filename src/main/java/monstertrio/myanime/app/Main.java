cf3a1087e5ee4b1636483900ac3c82889e1cbdb0
zacisyourmaster <zac15590@gmail.com>
2024-04-17 17:20:38 -0700

    App runs now

--- src/main/java/monstertrio/myanime/app/Main.java
+++ src/main/java/monstertrio/myanime/app/Main.java
@@ -5,41 +5,29 @@ import javafx.scene.Parent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import monstertrio.myanime.app.helpers.DatabaseHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.util.Objects;
 
+//Main Done
 public class Main extends Application {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
         try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/login.fxml")));
+            Scene scene = new Scene(root);
 
-            // Load scenes from FXML files
-            Parent loginRoot = FXMLLoader.load(getClass().getResource("login.fxml"));
-            Scene loginScene = new Scene(loginRoot);
-
-            Parent signupRoot = FXMLLoader.load(getClass().getResource("signup.fxml"));
-            Scene signUpScene = new Scene(signupRoot);
-
-            Parent animeListRoot = FXMLLoader.load(getClass().getResource("animeList.fxml"));
-            Scene animeListScene = new Scene(animeListRoot);
-
-            Parent animeAddRoot = FXMLLoader.load(getClass().getResource("animeAdd.fxml"));
-            Scene animeAddScene = new Scene(animeAddRoot);
-
-            // Set initial scene
-            primaryStage.setScene(loginScene);
-            primaryStage.setTitle("Anime Application - Login");
-            primaryStage.show();
-        } catch (Exception e){
-            logger.error("",e);
+            stage.setScene(scene);
+            stage.setTitle("MyAniTracker - Login");
+            stage.show();
+        } catch (Exception e) {
+            logger.error("An exception occurred!", e);
         }
     }
+
     public static void main(String[] args) {
         launch(args);
     }
