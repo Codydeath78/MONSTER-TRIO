3d2f12dabadb05a9d40a3854d842f2db5c2a7c1d
Codydeath78 <GOGETALIFEREALLYDUDE@gmail.com>
2024-04-18 11:15:07 -0700

    Work progress on Addcontroller.

--- src/main/java/monstertrio/myanime/app/controllers/AnimeAddController.java
+++ src/main/java/monstertrio/myanime/app/controllers/AnimeAddController.java
@@ -1,21 +1,69 @@
 package monstertrio.myanime.app.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import monstertrio.myanime.app.helpers.DatabaseHelper;
+import monstertrio.myanime.app.models.Anime;
+import monstertrio.myanime.app.models.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AnimeAddController {
+public class AnimeAddController{
+
+    private final DatabaseHelper helper;
+    private int userId;
+
+    public AnimeAddController(){
+         helper = new DatabaseHelper();
+    }
     public void setUserInformation(int userId) {
+        this.userId = userId;
     }
 
+    @FXML
+    public Button addButtonpage;
+    @FXML
+    private TableView<Anime> animeTableView;
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        DatabaseHelper helper = new DatabaseHelper();
+
+        addButtonpage.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+                Anime anime = new Anime(Anime.getTitle().getText(),Anime.getDesc().getText(),Anime.getRating().getText(),Anime.getStatus().getText(),Anime.getGenre().getText(),Anime.getUserId().getText());
+
+                add(anime);
+            }
+
+
+
+        });
+
+
+
+
+     }
+
+
+
+public void add(Anime anime){
+    animeTableView.getItems().add(anime);
+}
 /*
 
     private Scene scene;
