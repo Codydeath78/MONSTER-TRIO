d06905385e34f2b4ae84d07e493f43afddee3146
zacisyourmaster <zac15590@gmail.com>
2024-05-03 18:10:33 -0700

    finished ForgotPasswordController

--- src/main/java/monstertrio/myanime/app/controllers/ForgotPasswordController.java
+++ src/main/java/monstertrio/myanime/app/controllers/ForgotPasswordController.java
@@ -1,7 +1,6 @@
 package monstertrio.myanime.app.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,6 +9,7 @@ import javafx.scene.control.TextField;
 import monstertrio.myanime.app.helpers.DatabaseHelper;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ForgotPasswordController implements Initializable {
@@ -37,31 +37,33 @@ public class ForgotPasswordController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         label_message.setText("");
-        button_submit.setOnAction(new EventHandler<ActionEvent>() {
+        button_submit.setOnAction(actionEvent -> {
+            String username = tf_username.getText();
+            String userId = tf_user_id.getText();
+            String password = tf_new_password.getText();
 
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                String username = tf_username.getText();
-                String user_id = tf_user_id.getText();
-                String password = tf_new_password.getText();
-                if (username.isEmpty() || user_id.isEmpty() || password.isEmpty()) {
-                    label_message.setText("Fill all the fields!");
-                    label_message.setStyle("-fx-text-fill: red;");
-                } else {
-                    label_message.setText("Success! Redirecting to login page...");
-                    label_message.setStyle("-fx-text-fill: black;");
-                    DatabaseHelper.changeScene(actionEvent, "/views/Login.fxml", "MyAniTracker - Login", -1, 0);
-                }
-            }
-        });
-        button_login.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
+            if (username.isEmpty() || userId.isEmpty() || password.isEmpty()) {
+                label_message.setText("Fill all the fields!");
+                label_message.setStyle("-fx-text-fill: red;");
+            } else {
+               int intUserId = Integer.parseInt(userId);
+
                 try {
-                    DatabaseHelper.changeScene(actionEvent, "/views/Login.fxml", "MyAniTracker - Sign Up", -1, 0);
-                } catch (RuntimeException e) {
+                    helper.changeUserPassword(intUserId,username,password);
+                } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
+
+                label_message.setText("Success! Redirecting to login page...");
+                label_message.setStyle("-fx-text-fill: black;");
+                DatabaseHelper.changeScene(actionEvent, "/views/Login.fxml", "MyAniTracker - Login", -1, 0);
+            }
+        });
+        button_login.setOnAction(actionEvent -> {
+            try {
+                DatabaseHelper.changeScene(actionEvent, "/views/Login.fxml", "MyAniTracker - Sign Up", -1, 0);
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
             }
         });
 
